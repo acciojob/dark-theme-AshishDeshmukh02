1f5303156f2d986dedd66fc754724869a8ff484a
Ashish Deshmukh <115146337+AshishDeshmukh02@users.noreply.github.com>
2023-01-30 12:51:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
-//your code here
+function swapTheme() {
+  const app = document.getElementById("app");
+  const swapButton = document.getElementById("swap");
+  
+  if (app.classList.contains("day")) {
+    app.classList.remove("day");
+    app.classList.add("night");
+    swapButton.innerHTML = "Light Theme";
+    swapButton.classList.remove("button_day");
+    swapButton.classList.add("button_night");
+  } else {
+    app.classList.remove("night");
+    app.classList.add("day");
+    swapButton.innerHTML = "Dark Theme";
+    swapButton.classList.remove("button_night");
+    swapButton.classList.add("button_day");
+  }
+}
 
+document.getElementById("swap").addEventListener("click", swapTheme);
